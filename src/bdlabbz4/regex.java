2b99b489b5f8efe19bc518909f1af8bd1bcf65db
Shivaleela <shivaleelakatti13@gmail.com>
2023-11-24 15:19:53 +0530

    adding password with uppercase numeric special charcater of 8 character

--- src/bdlabbz4/regex.java
+++ src/bdlabbz4/regex.java
@@ -27,6 +27,7 @@ public class regex {
         //uc5:user valid password with 8 character
         //uc6:password having atleast one uppercase character
         //uc7:password having atleast one numeric character
+        //uc8:password having atleast one special character
         System.out.println("Enter password: ");
         String password = scan.nextLine();
 
@@ -34,7 +35,7 @@ public class regex {
         String nameRegex = "^[A-Z][a-zA-Z]{2,}$";
         String emailRegex = "^[A-Za-z0-9+_.-]+@gmail\\.com$";
         String phoneRegex = "^\\+[0-9]{1,3}[0-9]{10}$";
-        String passwordRegex = "^(?=.*[A-Z])(?=.*[a-z\\d])(?=.*\\d)[A-Za-z\\d]{8,}$";
+        String passwordRegex = "^(?=.*[A-Z])(?=.*[a-z\\d])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
         
         // Compile the regex patterns
         Pattern namePattern = Pattern.compile(nameRegex);
