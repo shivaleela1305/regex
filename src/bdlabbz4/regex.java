30d51dfd478f347ee0ddd74dccae405ee4e49c26
Shivaleela <shivaleelakatti13@gmail.com>
2023-11-24 15:00:35 +0530

    adding password with 8 character

--- src/bdlabbz4/regex.java
+++ src/bdlabbz4/regex.java
@@ -23,25 +23,32 @@ public class regex {
         //uc4:user phone  number with +and country code along with 10-digits number
         System.out.println("Enter phone number: ");
         String phoneNumber = scan.nextLine();
+        
+        //uc5:user valid password with 8 character
+        System.out.println("Enter password: ");
+        String password = scan.nextLine();
 
         // Define regex patterns for validation
         String nameRegex = "^[A-Z][a-zA-Z]{2,}$";
         String emailRegex = "^[A-Za-z0-9+_.-]+@gmail\\.com$";
         String phoneRegex = "^\\+[0-9]{1,3}[0-9]{10}$";
+        String passwordRegex = "^[a-z\\d]{8,}$";
         
         // Compile the regex patterns
         Pattern namePattern = Pattern.compile(nameRegex);
         Pattern emailPattern = Pattern.compile(emailRegex);
         Pattern phonePattern = Pattern.compile(phoneRegex);
+        Pattern passwordPattern = Pattern.compile(passwordRegex);
         
         // Match the input credentials against the regex patterns
         Matcher firstNameMatcher = namePattern.matcher(firstName);
         Matcher lastNameMatcher = namePattern.matcher(lastName);
         Matcher emailMatcher = emailPattern.matcher(email);
         Matcher phoneMatcher = phonePattern.matcher(phoneNumber);
+        Matcher passwordMatcher = passwordPattern.matcher(password);
         
         // Check if the credentials match the required patterns
-        if (firstNameMatcher.matches() && lastNameMatcher.matches() && emailMatcher.matches() && phoneMatcher.matches()) {
+        if (firstNameMatcher.matches() && lastNameMatcher.matches() && emailMatcher.matches() && phoneMatcher.matches()  && passwordMatcher.matches()) {
             System.out.println("Valid credentials. field meets the requirements.");
         } else {
             if (!firstNameMatcher.matches()) {
@@ -55,6 +62,9 @@ public class regex {
             }
             if (!phoneMatcher.matches()) {
                 System.out.println("Invalid phone number. It should start with a '+' followed by a country code and then a 10-digit number.");
+            }
+            if (!passwordMatcher.matches()) {
+                System.out.println("Invalid password. It should meet the specified requirements.");
             }
         }
 	}
