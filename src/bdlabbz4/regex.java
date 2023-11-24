ed88fe2f922e3f86a8fe02eec527307ade42a4d1
Shivaleela <shivaleelakatti13@gmail.com>
2023-11-24 14:26:20 +0530

    adding lastname with uppercase

--- src/bdlabbz4/regex.java
+++ src/bdlabbz4/regex.java
@@ -11,6 +11,11 @@ public class regex {
         //uc1:user first name starts with capital letter and have minimum 3-character
         System.out.print("Enter first name: ");
         String firstName = scan.nextLine();
+        
+      //uc2:user last name starts with capital letter and have minimum 3-character
+        System.out.println("Enter last name: ");
+        String lastName = scan.nextLine();
+        
 
         // Define regex patterns for validation
         String nameRegex = "^[A-Z][a-zA-Z]{2,}$";
@@ -20,13 +25,17 @@ public class regex {
 
         // Match the input credentials against the regex patterns
         Matcher firstNameMatcher = namePattern.matcher(firstName);
+        Matcher lastNameMatcher = namePattern.matcher(lastName);
 
         // Check if the credentials match the required patterns
-        if (firstNameMatcher.matches() ) {
+        if (firstNameMatcher.matches() && lastNameMatcher.matches() ) {
             System.out.println("Valid credentials. field meets the requirements.");
         } else {
             if (!firstNameMatcher.matches()) {
                 System.out.println("Invalid first name. It should start with a capital letter and have a minimum of 3 characters.");
+            }
+            if (!lastNameMatcher.matches()) {
+                System.out.println("Invalid last name. It should start with a capital letter and have a minimum of 3 characters.");
             }
         }
 	}
